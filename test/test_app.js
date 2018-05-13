316c61223ce002b31539a889a5f5752ed2636b5c
Dave Brown <david.brown@nominum.com>
2018-05-13 01:25:02 -0500

    Use correct expect statements in tests
    This clears up the issue where the arrays are not compared correctly.

--- test/test_app.js
+++ test/test_app.js
@@ -23,12 +23,9 @@ describe('files', function() {
     });
 
     it('should return a json list', function(done) {
-        // todo : this didn't fail when the list was incomplete
         request(app)
             .get('/files')
             .set('Accept', 'application/json')
-            .expect(['.idea', 'app.js', 'bin', 'main.js', 'node_modules', 'package-lock.json',
-                'package.json', 'public', 'routes', 'test', 'watcher.js'])
             .end(function(err, resp) {  // note to self - this HAPPENS at the end, not ends the call
                 chai.expect(resp.statusCode).to.equal(200);
                 chai.expect(resp.header['content-type']).to.include('application/json');
@@ -40,30 +37,26 @@ describe('files', function() {
     });
 
     it('should return a filtered json list from a get', function(done) {
-        // todo : this didn't fail when the list was incomplete
         request(app)
             .get('/files/pa')
             .set('Accept', 'application/json')
-            .expect(['package-lock.json', 'package.json' ])  // todo check length?
             .end(function(err, resp) {  // note to self - this HAPPENS at the end, not ends the call
                 chai.expect(resp.statusCode).to.equal(200);
                 chai.expect(resp.header['content-type']).to.include('application/json');
-                // todo public not in ...
+                chai.expect(resp.body).to.eql( ['package-lock.json', 'package.json'] );
                 done();
             });
     });
 
     it('should return a filtered json list from a post', function(done) {
-        // todo : this didn't fail when the list was incomplete
         request(app)
             .post('/files')
-            .send('pa')
+            .send({'prefix':'pa'})
             .set('Accept', 'application/json')
-            .expect(['package-lock.json', 'package.json' ])  // todo check length?
             .end(function(err, resp) {  // note to self - this HAPPENS at the end, not ends the call
                 chai.expect(resp.statusCode).to.equal(200);
                 chai.expect(resp.header['content-type']).to.include('application/json');
-                // todo public not in ...
+                chai.expect(resp.body).to.eql( ['package-lock.json', 'package.json'] );
                 done();
             });
     });
@@ -72,12 +65,11 @@ describe('files', function() {
         request(app)
             .get('/files/paradise_island_would_be_a_great_place_about_now')
             .set('Accept', 'application/json')
-            .expect(['package-lock.json', 'package.json' ])  // todo check length?
             //.expect('public').not.in.something
             .end(function(err, resp) {
                 chai.expect(resp.statusCode).to.equal(200);
                 chai.expect(resp.header['content-type']).to.include('application/json');
-                // todo public not in ...
+                chai.expect(resp.body).to.eql( [] );
                 done();
             });
     });
